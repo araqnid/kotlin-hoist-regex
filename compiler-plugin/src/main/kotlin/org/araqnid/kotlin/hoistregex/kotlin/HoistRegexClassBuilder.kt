5374c31776615c10204ba19f1c0ca6720673c270
Steve Haslam <araqnid@googlemail.com>
2020-03-21 20:22:58 +0000

    Remove restriction on looking inside function definitions

--- compiler-plugin/src/main/kotlin/org/araqnid/kotlin/hoistregex/kotlin/HoistRegexClassBuilder.kt
+++ compiler-plugin/src/main/kotlin/org/araqnid/kotlin/hoistregex/kotlin/HoistRegexClassBuilder.kt
@@ -2,7 +2,6 @@ package org.araqnid.kotlin.hoistregex.kotlin
 
 import org.jetbrains.kotlin.codegen.ClassBuilder
 import org.jetbrains.kotlin.codegen.DelegatingClassBuilder
-import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.resolve.jvm.diagnostics.JvmDeclarationOrigin
 import org.jetbrains.org.objectweb.asm.MethodVisitor
 import org.jetbrains.org.objectweb.asm.Opcodes
@@ -21,14 +20,13 @@ class HoistRegexClassBuilder(private val delegateBuilder: ClassBuilder) : Delega
         exceptions: Array<out String>?
     ): MethodVisitor {
         val original = super.newMethod(origin, access, name, desc, signature, exceptions)
-        val function = origin.descriptor as? FunctionDescriptor ?: return original
         return object : MethodVisitor(Opcodes.ASM5, original) {
             override fun visitTypeInsn(opcode: Int, type: String?) {
                 when (opcode) {
                     NEW -> {
                         InstructionAdapter(this).apply {
                             if (type == "kotlin/text/Regex") {
-                                println("seen Regex creation: origin.originKind=${origin.originKind} access=$access method=$name type=$type")
+                                println("seen Regex creation: origin.descriptor=${origin.descriptor} access=$access method=$name type=$type")
                             }
                         }
                     }
--- compiler-plugin/src/testInput/kotlin/testInput/Example.kt
+++ compiler-plugin/src/testInput/kotlin/testInput/Example.kt
@@ -1,7 +1,23 @@
 package testInput
 
 class Example {
+    val pattern = Regex("""\S+""")
+
     fun someMethod(input: String): Boolean {
         return input.matches(Regex("""\S+"""))
     }
+
+    companion object {
+        // don't intercept this
+        val staticPattern = Regex("""\S+""")
+    }
 }
+
+object Example2 {
+    // don't intercept this
+    val objectPattern = Regex("""\S+""")
+
+}
+
+// don't intercept this
+val toplevelPattern = Regex("""\S+""")
